3b8b8ea80b208633e54389139ee9303d429e1939
egyaranda02 <egyaranda02@gmail.com>
2021-10-22 18:30:27 +0700

    add CheckAdmin Middleware

--- app/middleware/authMiddleware.js
+++ app/middleware/authMiddleware.js
@@ -6,7 +6,7 @@ const { Op } = require("sequelize");
 const checkLogin = (req, res, next) =>{
     const token = req.cookies.jwt;
     if(!token){
-        return res.status(200).json({
+        return res.status(401).json({
             success: false,
             message: "You aren't logged in"
         })
@@ -14,4 +14,16 @@ const checkLogin = (req, res, next) =>{
     next();
 }
 
-module.exports = {checkLogin}
+const checkAdmin = (req, res, next)=>{
+    const token = req.cookies.jwt;
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    if(!decoded.AdminId){
+        return res.status(401).json({
+            success: false,
+            message: "You are not an Admin"
+        })
+    }
+    next();
+}
+
+module.exports = {checkLogin, checkAdmin}
